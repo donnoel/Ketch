8da76d2cc624e9ab8e9869f52a12a57523a16a6a
Don Noel <78627833+donnoel@users.noreply.github.com>
2026-05-23 13:55:38 -0700

    push

--- Ketch/GameScene.swift
+++ Ketch/GameScene.swift
@@ -1,109 +1,189 @@
-//
-//  GameScene.swift
-//  Ketch
-//
-//  Created by Don Noel on 5/23/26.
-//
-
 import SpriteKit
-import GameplayKit
 
-class GameScene: SKScene {
-    
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
-    override func sceneDidLoad() {
+final class GameScene: SKScene, SKPhysicsContactDelegate {
 
-        self.lastUpdateTime = 0
-        
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
+    private enum PhysicsCategory {
+        static let player: UInt32 = 1 << 0
+        static let fallingObject: UInt32 = 1 << 1
+        static let ground: UInt32 = 1 << 2
+    }
+
+    private let player = SKSpriteNode(color: .systemBlue, size: CGSize(width: 72, height: 32))
+    private let scoreLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
+    private let gameOverLabel = SKLabelNode(fontNamed: "AvenirNext-Bold")
+
+    private var score = 0 {
+        didSet {
+            scoreLabel.text = "Score: \(score)"
         }
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
+
+    private var isGameOver = false
+
+    override func didMove(to view: SKView) {
+        setupScene()
+        setupPhysics()
+        setupPlayer()
+        setupLabels()
+        startSpawningObjects()
     }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+
+    private func setupScene() {
+        backgroundColor = .black
+        scaleMode = .resizeFill
     }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
+
+    private func setupPhysics() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: -2.5)
+        physicsWorld.contactDelegate = self
+
+        let ground = SKNode()
+        ground.position = CGPoint(x: size.width / 2, y: 24)
+        ground.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: size.width, height: 10))
+        ground.physicsBody?.isDynamic = false
+        ground.physicsBody?.categoryBitMask = PhysicsCategory.ground
+        ground.physicsBody?.contactTestBitMask = PhysicsCategory.fallingObject
+        ground.physicsBody?.collisionBitMask = 0
+
+        addChild(ground)
     }
-    
+
+    private func setupPlayer() {
+        player.position = CGPoint(x: size.width / 2, y: 90)
+        player.physicsBody = SKPhysicsBody(rectangleOf: player.size)
+        player.physicsBody?.isDynamic = false
+        player.physicsBody?.categoryBitMask = PhysicsCategory.player
+        player.physicsBody?.contactTestBitMask = PhysicsCategory.fallingObject
+        player.physicsBody?.collisionBitMask = 0
+
+        addChild(player)
+    }
+
+    private func setupLabels() {
+        scoreLabel.text = "Score: 0"
+        scoreLabel.fontSize = 28
+        scoreLabel.fontColor = .white
+        scoreLabel.horizontalAlignmentMode = .left
+        scoreLabel.position = CGPoint(x: 24, y: size.height - 70)
+
+        addChild(scoreLabel)
+
+        gameOverLabel.text = "Game Over\nTap to Restart"
+        gameOverLabel.fontSize = 36
+        gameOverLabel.fontColor = .white
+        gameOverLabel.numberOfLines = 2
+        gameOverLabel.horizontalAlignmentMode = .center
+        gameOverLabel.verticalAlignmentMode = .center
+        gameOverLabel.position = CGPoint(x: size.width / 2, y: size.height / 2)
+        gameOverLabel.isHidden = true
+
+        addChild(gameOverLabel)
+    }
+
+    private func startSpawningObjects() {
+        let spawn = SKAction.run { [weak self] in
+            self?.spawnFallingObject()
+        }
+
+        let wait = SKAction.wait(forDuration: 1.0)
+        let sequence = SKAction.sequence([spawn, wait])
+        let repeatForever = SKAction.repeatForever(sequence)
+
+        run(repeatForever, withKey: "spawningObjects")
+    }
+
+    private func spawnFallingObject() {
+        guard !isGameOver else { return }
+
+        let object = SKShapeNode(circleOfRadius: 18)
+        object.fillColor = .systemYellow
+        object.strokeColor = .clear
+
+        let randomX = CGFloat.random(in: 40...(size.width - 40))
+        object.position = CGPoint(x: randomX, y: size.height + 40)
+
+        object.physicsBody = SKPhysicsBody(circleOfRadius: 18)
+        object.physicsBody?.categoryBitMask = PhysicsCategory.fallingObject
+        object.physicsBody?.contactTestBitMask = PhysicsCategory.player | PhysicsCategory.ground
+        object.physicsBody?.collisionBitMask = 0
+
+        addChild(object)
+    }
+
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
+        guard let touch = touches.first else { return }
+
+        if isGameOver {
+            restartGame()
+            return
         }
-        
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+
+        let location = touch.location(in: self)
+        movePlayer(toX: location.x)
     }
-    
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
+
+    private func movePlayer(toX xPosition: CGFloat) {
+        let halfWidth = player.size.width / 2
+        let clampedX = min(max(xPosition, halfWidth), size.width - halfWidth)
+
+        let moveAction = SKAction.moveTo(x: clampedX, duration: 0.16)
+        moveAction.timingMode = .easeOut
+
+        player.run(moveAction)
     }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    
-    override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
+
+    func didBegin(_ contact: SKPhysicsContact) {
+        let categories = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
+
+        if categories == PhysicsCategory.player | PhysicsCategory.fallingObject {
+            handleCatch(contact)
+        } else if categories == PhysicsCategory.ground | PhysicsCategory.fallingObject {
+            handleMiss(contact)
         }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
+    }
+
+    private func handleCatch(_ contact: SKPhysicsContact) {
+        let object = fallingObject(from: contact)
+        object?.removeFromParent()
+
+        score += 1
+    }
+
+    private func handleMiss(_ contact: SKPhysicsContact) {
+        let object = fallingObject(from: contact)
+        object?.removeFromParent()
+
+        endGame()
+    }
+
+    private func fallingObject(from contact: SKPhysicsContact) -> SKNode? {
+        if contact.bodyA.categoryBitMask == PhysicsCategory.fallingObject {
+            return contact.bodyA.node
         }
-        
-        self.lastUpdateTime = currentTime
+
+        if contact.bodyB.categoryBitMask == PhysicsCategory.fallingObject {
+            return contact.bodyB.node
+        }
+
+        return nil
+    }
+
+    private func endGame() {
+        isGameOver = true
+        removeAction(forKey: "spawningObjects")
+        gameOverLabel.isHidden = false
+    }
+
+    private func restartGame() {
+        removeAllChildren()
+        removeAllActions()
+
+        score = 0
+        isGameOver = false
+
+        setupPhysics()
+        setupPlayer()
+        setupLabels()
+        startSpawningObjects()
     }
 }
